e7d5b3feef9081febbddccc49c8df122560e7301
Gabriel Luiz Batista Passos <gabrielluiz.passos@gmail.com>
2026-03-25 20:48:33 -0300

    W04 Assignment: YouTube Video Program (Foundation #1)

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,9 +1,53 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        List<Video> videos = new List<Video>();
+
+        Video video1 = new Video("C# Classes for Beginners", "Code Academy", 420);
+        video1.AddComment(new Comment("Alice", "This explanation was really clear."));
+        video1.AddComment(new Comment("Bruno", "Now I finally understand classes."));
+        video1.AddComment(new Comment("Carla", "Great examples, thanks!"));
+        video1.AddComment(new Comment("Daniel", "Please make one about inheritance."));
+        videos.Add(video1);
+
+        Video video2 = new Video("Top 10 Study Tips", "Student Success", 300);
+        video2.AddComment(new Comment("Emma", "These tips are very helpful."));
+        video2.AddComment(new Comment("Felipe", "I will try the time blocking idea."));
+        video2.AddComment(new Comment("Giovana", "Nice video and easy to follow."));
+        videos.Add(video2);
+
+        Video video3 = new Video("Healthy Morning Routine", "Life Balance", 515);
+        video3.AddComment(new Comment("Hugo", "I need to improve my mornings."));
+        video3.AddComment(new Comment("Isabela", "The advice about sleep was great."));
+        video3.AddComment(new Comment("João", "Simple and motivating."));
+        video3.AddComment(new Comment("Karen", "I started doing this routine too."));
+        videos.Add(video3);
+
+        Video video4 = new Video("How to Build a Portfolio", "Dev Career", 610);
+        video4.AddComment(new Comment("Lucas", "This helped me a lot."));
+        video4.AddComment(new Comment("Marina", "Very useful for junior developers."));
+        video4.AddComment(new Comment("Nicolas", "Can you make a resume video next?"));
+        videos.Add(video4);
+
+        foreach (Video video in videos)
+        {
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine($"Title: {video.Title}");
+            Console.WriteLine($"Author: {video.Author}");
+            Console.WriteLine($"Length (seconds): {video.LengthInSeconds}");
+            Console.WriteLine($"Number of comments: {video.GetNumberOfComments()}");
+            Console.WriteLine("Comments:");
+
+            foreach (Comment comment in video.GetComments())
+            {
+                Console.WriteLine($"- {comment.CommenterName}: {comment.Text}");
+            }
+
+            Console.WriteLine();
+        }
     }
 }
